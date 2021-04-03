3dd993d191cdaaefa97d1dec5cc37ea3a59066af
kotsonis <stefankotsonis@gmail.com>
2021-04-03 18:05:00 +0200

    fix: move S3 bucket into serverless.ts

--- backend/serverless.ts
+++ backend/serverless.ts
@@ -4,7 +4,7 @@ import getTodos from '@functions/http/getTodos';
 import createTodo from '@functions/http/createTodo';
 import generateUploadUrl from '@functions/http/generateUploadUrl'
 import auth0Authorizer from '@functions/auth/auth0Authorizer'
-import {BucketPolicy, AttachmentsBucket} from 'src/resources/s3'
+// import {BucketPolicy, AttachmentsBucket} from 'src/resources/s3'
 
 const serverlessConfiguration: AWS = {
   service: 'serverless-todo-app',
@@ -93,8 +93,54 @@ const serverlessConfiguration: AWS = {
         ],
       },
     },
-    AttachmentsBucket,
-    BucketPolicy 
+    AttachmentsBucket: {
+      Type: "AWS::S3::Bucket",
+      Properties: {
+        BucketName: "${self:provider.environment.TODOS_S3_BUCKET}",
+        CorsConfiguration: {
+          CorsRules: [
+            {
+              AllowedOrigins: ["*"],
+              AllowedHeaders: ["*"],
+              AllowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+              MaxAge: 3000,
+            },
+          ],
+        },
+      },
+    },
+    BucketPolicy: {
+      Type: "AWS::S3::BucketPolicy",
+      Properties: {
+        Bucket: {
+          Ref: "AttachmentsBucket",
+        },
+        PolicyDocument: {
+          Id: "MyPolicy",
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Sid: "PublicReadForGetBucketObjects",
+              Effect: "Allow",
+              Principal: "*",
+              Action: ["s3:GetObject"],
+              Resource: {
+                "Fn::Join": [
+                  "",
+                  [
+                    "arn:aws:s3:::",
+                    {
+                      Ref: "AttachmentsBucket",
+                    },
+                    "/*",
+                  ],
+                ],
+              },
+            },
+          ],
+        },
+      },
+    }
   },
 }
 };

515fd71b01885d5ba66b354bd6522cbd2fc74692
Stefanos Kotsonis <61590063+kotsonis@users.noreply.github.com>
2021-04-05 19:01:20 +0200

    Update config.ts with main branch apiId

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'jp2dweoip0'
+const apiId = 'zdk392skkd'
 const region = 'eu-central-1'
-export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/main`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
